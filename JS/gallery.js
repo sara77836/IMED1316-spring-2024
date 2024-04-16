e34bc3913c21fc0994ff8402143e81a325c727d4
sara77836 <157166359+sara77836@users.noreply.github.com>
2024-04-16 14:57:45 -0500

    manual thumbnails

--- JS/gallery.js
+++ JS/gallery.js
@@ -3,9 +3,20 @@ $( document ).ready(function() {
    //console.log( "ready!" );
 
    $("#blinn-gallery").nanogallery2({
-    ...
-    thumbnailHeight:  200,
-    thumbnailWidth:   200
+        thumbnailWidth: "300",
+        thubmnailHeight: "auto",
+        thumbnailBorderVertical: 0,
+        thumbnailBorderHorizontal: 0,
+        thumbnailLabel: {
+            postition: "overImageOnBottom",
+            display: false
+        },
+        thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|labelAppear75",
+        thumbnailGutterWidth: 0,
+        thumbnailGutterHeight: 0,
+        thumbnailAlignment: "center",
+        thumbnailOpenImage: true
+
   });
 
 
